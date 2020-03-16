de4f48ddefe5df8c1bf012696e523e9864cee308
Kyle Shepard <kshepard379@gmail.com>
2020-03-16 16:29:54 -0700

    now compiles data from JSON file to prepare to write to csv

--- src/APICall.java
+++ src/APICall.java
@@ -1,3 +1,5 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -15,56 +17,110 @@ public class APICall implements Runnable{
     public HttpClient client;
     public HttpRequest request;
     HttpResponse<String> response;
+    String stopNumber;
+    String stopName;
+    String APIKey;
 
-    public APICall(String uri){
+    /**
+     * 
+     * @param uri - partial url for api call to Puget Sound OneBusAway
+     * @param stopNumber - Stop number not including the "1_"
+     * @param stopName - address/name for bus stop
+     * @param APIKey - api key used for call
+     */
+    public APICall(String uri, String stopNumber, String stopName, String APIKey){
 
         client = HttpClient.newHttpClient();
         //test api call - remove
         request = HttpRequest.newBuilder()
-        .uri(URI.create(uri))
-        .timeout(Duration.ofSeconds(30))
+        .uri(URI.create(uri  + stopNumber + ".json?key=" + APIKey)) //we want a .json file for our json.simple library
+        .timeout(Duration.ofSeconds(15))
         .build();
+        this.stopNumber = stopNumber;
+        this.stopName = stopName;
     }
 
+    public void printEntry(String[] stopData) throws IOException {
+    	FileWriter pw = new FileWriter("",true);
+    }
+    
     public void run(){
 
         try{
         	//initialize HttpClient to call api call
-            response = client.send(request, BodyHandlers.ofString());
+        	int count = 0;
+            do {
+            	if (count > 3) {
+            		break;
+            	}
+            	response = client.send(request, BodyHandlers.ofString());
+            	count++;
+            	
+
+            } while (response.body().contentEquals(""));	//try again if timeout
             
             //time variables
-            int current;
-            int predicted;
-            int scheduled;
+            Long current;
+            Long predicted;
+            Long scheduled;
             
-            //
+            //grab arrival information for current stop
             JSONParser parser = new JSONParser();
+
             JSONObject result = (JSONObject)parser.parse(response.body());
-            current = Integer.parseInt(result.get("currentTime").toString()) / 60000;
+            current = Long.parseLong(result.get("currentTime").toString()) / 60000;
             result = (JSONObject) result.get("data");
             result = (JSONObject) result.get("entry");
             JSONArray arrivals = (JSONArray) result.get("arrivalsAndDepartures");
             
+            //only continue if there are any routes returned in the first place to grab data from
             if(arrivals.size() > 0) {
-            	JSONObject arrivalElements = (JSONObject) arrivals.get(0);
-            	predicted = Integer.parseInt(arrivalElements.get("predictedArrivalTime").toString()) / 60000;
-            	scheduled = Integer.parseInt(arrivalElements.get("scheduledArrivalTime").toString()) / 60000;
-            }
-            
-            //int predicted = ((int) finalObj.get("predictedArrivalTime")) / 60000;
-            //int scheduled = ((int) finalObj.get("scheduledArrivalTime")) / 60000;            
-            
-            for (int i = 0; i < arrivals.size(); i++) {
-            	
-                //System.out.print(i + ": " + arrivalsAndDepartures.get(i));
-            }
 
-            //System.out.println(arrivalsAndDepartures.toString());
-
-            //System.out.println(response.body());
-        } catch (Exception ex){
-
-            System.out.println("whoops!");
+            	//loop through each returned arrival
+            	for (int i = 0; i < arrivals.size(); i++) {
+            		
+            		//get timestamp data for each arrival to check when a bus is nearby the stop
+            		JSONObject arrivalElements = (JSONObject) arrivals.get(i);
+            		predicted = Long.parseLong(arrivalElements.get("predictedArrivalTime").toString()) / 60000;
+            		scheduled = Long.parseLong(arrivalElements.get("scheduledArrivalTime").toString()) / 60000;
+            		String route = arrivalElements.get("routeShortName").toString();
+            		
+            		//if bus is here NOW (predicted time in minutes equals current time in minute)
+            		if((predicted - current) == 0) {
+            			
+            			//keep track if bus was late/on time/ early, and by how much
+            			Long punctuality = (predicted - scheduled);
+            			String onTimeStatus;
+            			
+            			if (punctuality == 0) {
+            				onTimeStatus = "On Time";
+            			} else if (punctuality > 0) {
+            				onTimeStatus = "Late";
+            			} else {
+            				onTimeStatus = "Early";
+            			}
+            			
+                    	// 0-stop number, 1-stop name, 2-route number, 3-on time status, 4-how late/early
+            			String[] stopData = {stopNumber, stopName, route, onTimeStatus, punctuality.toString()};
+            			
+            			//TODO check if entry is duplicate
+            			
+            			for(String s: stopData) {
+            				System.out.println(s);
+            			}
+            			System.out.println();
+            		}
+            	}
+            }       
+        }
+        catch (ParseException pe) {
+        	System.out.println("Tried calling api for stop# " + stopNumber + " 3 times with no server response.");
+        }
+        catch (IOException ioex) {
+        	System.out.println("Failed to write to file");
+        }
+        catch (Exception ex){
+            System.out.println("Something went wrong: " + ex.toString() + "\n");
             //cry
         }
     }
