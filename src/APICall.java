597e9568fa51feb27d50b5dcff34064ed8b0ae2d
Kyle Shepard <kshepard379@gmail.com>
2020-03-15 21:01:50 -0700

    Update APICall.java

--- src/APICall.java
+++ src/APICall.java
@@ -29,15 +29,37 @@ public class APICall implements Runnable{
     public void run(){
 
         try{
-        
+        	//initialize HttpClient to call api call
             response = client.send(request, BodyHandlers.ofString());
+            
+            //time variables
+            int current;
+            int predicted;
+            int scheduled;
+            
+            //
             JSONParser parser = new JSONParser();
             JSONObject result = (JSONObject)parser.parse(response.body());
-            JSONObject data = (JSONObject) result.get("data");
-            JSONObject entry = (JSONObject) data.get("entry");
-            JSONArray arrivalsAndDepartures = (JSONArray) entry.get("arrivalsAndDepartures");
+            current = Integer.parseInt(result.get("currentTime").toString()) / 60000;
+            result = (JSONObject) result.get("data");
+            result = (JSONObject) result.get("entry");
+            JSONArray arrivals = (JSONArray) result.get("arrivalsAndDepartures");
+            
+            if(arrivals.size() > 0) {
+            	JSONObject arrivalElements = (JSONObject) arrivals.get(0);
+            	predicted = Integer.parseInt(arrivalElements.get("predictedArrivalTime").toString()) / 60000;
+            	scheduled = Integer.parseInt(arrivalElements.get("scheduledArrivalTime").toString()) / 60000;
+            }
+            
+            //int predicted = ((int) finalObj.get("predictedArrivalTime")) / 60000;
+            //int scheduled = ((int) finalObj.get("scheduledArrivalTime")) / 60000;            
+            
+            for (int i = 0; i < arrivals.size(); i++) {
+            	
+                //System.out.print(i + ": " + arrivalsAndDepartures.get(i));
+            }
 
-            System.out.println(arrivalsAndDepartures.toString());
+            //System.out.println(arrivalsAndDepartures.toString());
 
             //System.out.println(response.body());
         } catch (Exception ex){
