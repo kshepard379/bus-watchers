114409e8af48eb721b5eb09aa89a1e8e1201125e
Kyle Shepard <kshepard379@gmail.com>
2020-03-15 19:43:24 -0700

    added basic JSON parsing

--- src/APICall.java
+++ src/APICall.java
@@ -4,7 +4,11 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.time.Duration;
-//import org.json.simple.JSONArray;
+import java.util.Scanner;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
 
 public class APICall implements Runnable{
 
@@ -27,7 +31,15 @@ public class APICall implements Runnable{
         try{
         
             response = client.send(request, BodyHandlers.ofString());
-            System.out.println(response.body());
+            JSONParser parser = new JSONParser();
+            JSONObject result = (JSONObject)parser.parse(response.body());
+            JSONObject data = (JSONObject) result.get("data");
+            JSONObject entry = (JSONObject) data.get("entry");
+            JSONArray arrivalsAndDepartures = (JSONArray) entry.get("arrivalsAndDepartures");
+
+            System.out.println(arrivalsAndDepartures.toString());
+
+            //System.out.println(response.body());
         } catch (Exception ex){
 
             System.out.println("whoops!");
