f9aceb52bb893e2f934b6ea403049e6c6ffd428b
Kyle Shepard <kshepard379@gmail.com>
2020-03-17 15:12:15 -0700

    now runs threads once, and they launch their own scheduledpool

--- src/BusWatcher.java
+++ src/BusWatcher.java
@@ -5,7 +5,6 @@ import java.util.concurrent.TimeUnit;
 public class BusWatcher{
     public static void main(String[] args) throws InterruptedException{
 
-    	boolean runEnabled = true;
         String APIKey = "TEST";
         String[] stopNumbers = {"1_68004", "1_67655", "1_67652", "1_68007", "1_640", "1_690", "1_700", "1_81755", "1_58114", "1_361", "1_80432", "1_620", "29_2114"};
         String[] stopNames = {"Bellevue TC Bay 4", "Bellevue TC Bay 8", "Bellevue TC Bay 9", "Bellevue TC Bay 12", "4th Ave & James Street",
@@ -16,11 +15,12 @@ public class BusWatcher{
         
         long timeInterval = 30000 / stopNumbers.length;
 
-
         for(int i = 0; i < stopNumbers.length; i++){
            	//System.out.println(i + ": ");
-          	scheduledPool.scheduleWithFixedDelay(new APICall("http://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-stop/", stopNumbers[i], stopNames[i], APIKey), 0, 30, TimeUnit.SECONDS);
-            TimeUnit.MILLISECONDS.sleep(timeInterval);
+          	//scheduledPool.scheduleWithFixedDelay(new APICall("http://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-stop/", stopNumbers[i], stopNames[i], APIKey), 0, 30, TimeUnit.SECONDS);
+            APICall apicall = new APICall("http://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-stop/", stopNumbers[i], stopNames[i], APIKey);
+            apicall.run();
+        	TimeUnit.MILLISECONDS.sleep(timeInterval);
             //wait
         }
     }
